e5546c15b31ba207e0103a7d1fc6c5121861c3fc
Arnab Das <arnabdas.619@gmail.com>
2021-07-01 18:12:36 +0530

    aded Tags for recipe and serializers

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,5 +1,11 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from core import models
+
+
+def sample_user(email='test@gmail.com', password='testing1234'):
+    """creating a sample user for testing """
+    return get_user_model().objects.create_user(email, password)
 
 
 class ModelTests(TestCase):
@@ -41,3 +47,11 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        """test that tags are returned and as strings """
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Veg'
+        )
+        self.assertEqual(str(tag), tag.name)
